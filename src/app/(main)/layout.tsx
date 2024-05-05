9ebf709a1b04780f636823ff508965a25f19514e
Aaron-p-saji <aaronp.saji1233@gmail.com>
2024-05-05 12:17:33 +0530

    Cluttered

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = { children: React.ReactNode };
+
+const Layout = (props: Props) => {
+  return (
+    <div className='flex overflow-hidden h-screen'>
+      <div className='w-ful'>{props.children}</div>
+    </div>
+  );
+};
