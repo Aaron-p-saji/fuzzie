395c04da5ad4bbfe5e5620d20f92eb78aebece39
Aaron-p-saji <aaronp.saji1233@gmail.com>
2024-05-06 17:51:02 +0530

    Fixed Auth Issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
 const isPublicRoute = createRouteMatcher([
   "/",
   "/api/clerk-webhook",
   "/api/drive-activity/notifications",
 ]);
 
-export default clerkMiddleware(
-  (auth, req) => {
-    if (isPublicRoute(req)) return; // if it's a public route, do nothing
-    auth().protect(); // for any other route, require auth
-  },
-  { debug: true }
-);
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+  else if (isPublicRoute(req)) return;
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
